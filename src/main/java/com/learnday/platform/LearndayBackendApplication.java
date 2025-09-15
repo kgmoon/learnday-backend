3441559f10f847cd1d689dca00058b81081ffe70
MoonJeong <dev.moonjeong@gmail.com>
2025-09-15 18:59:14 +0900

    chore: .gitignore 파일 설정

---
+++ src/main/java/com/learnday/platform/LearndayBackendApplication.java
@@ -0,0 +1,16 @@
+package com.learnday.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearndayBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearndayBackendApplication.class, args);
+
+    }
+
+}
+
+
---
+++ src/test/java/com/learnday/platform/LearndayBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learnday.platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearndayBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
